6a07047be3f67b7531c5e3aa07e4b2cdc6bd304d
DhanshreeK <dhanshree.j.kamble@gmail.com>
2018-01-23 23:31:16 +0530

    added color on keyup and ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, style } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app/app.component.html'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  name = 'Ward';
+  title = 'Customer App';
+  wardsColor = 'blue'
+
+  changeSuitColor(){
+    this.wardsColor = this.wardsColor == 'blue' ?  'red' : 'blue';
+  }
+ }
